544705dd5d2c0e3502d2b0598b73e62adb81de4c
James Gohl <james8hg@gmail.com>
2025-03-10 22:19:36 -0400

    middle of lab 10

--- Lab 10 SYSC 2004/tictactoe/tictactoe/Main.java
+++ Lab 10 SYSC 2004/tictactoe/tictactoe/Main.java
@@ -1,3 +1,8 @@
 public class Main
 {
+public static void main(String[] args)
+{
+    TicTacToeFrame my_game = new TicTacToeFrame();
+    my_game.playGame();
+}
 }
---
+++ Lab 10 SYSC 2004/tictactoe/tictactoe/TicTacToeFrame.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ * A class modelling a tic-tac-toe (noughts and crosses, Xs and Os) game in a very
+ * simple GUI window.
+ * 
+ * @author Lynn Marshall
+ * @version November 8, 2012
+ */
+
+public class TicTacToeFrame extends TicTacToe 
+{ 
+   private JTextArea status; // text area to print game status
+   private JFrame frame;
+   private JScrollPane scroll;
+   
+   /** 
+    * Constructs a new Tic-Tac-Toe board and sets up the basic
+    * JFrame containing a JTextArea in a JScrollPane GUI.
+    */
+   public TicTacToeFrame()
+   { 
+       frame = new JFrame("Tic Tac Toe Game");
+       frame.setSize(400, 300);
+       status = new JTextArea(10, 30);
+       scroll = new JScrollPane(status);
+
+       frame.add(scroll, BorderLayout.CENTER);
+       frame.setVisible(true);
+   }
+   
+   /**
+    * Prints the board to the GUI using toString().
+    */
+    public void print() 
+    {  
+        status.append(super.toString() + "\n");
+    }
+
+}
